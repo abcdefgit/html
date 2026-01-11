b6120c0421fe5b5d06783a2554c139f4fb94eafe
abcdefgit <251744937+abcdefgit@users.noreply.github.com>
2026-01-11 20:50:51 +0900

    refactor: separate CSS and JS into external files / CSS 및 JS 코드를 외부 파일로 분리

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const btn = document.getElementById('myButton');
+const title = document.getElementById('title');
+
+btn.addEventListener('click', function () {
+    alert('Hello World!');
+    title.style.color = 'red';
+
+})
